5f907a557db285560663b9e49c27ea239b93bf08
Jen Annoh <jannoh@ed.ac.uk>
2026-05-08 10:15:37 +0100

    Delete redundant test_code.py. New test script in tests/ dir

--- test_code.py
+++
@@ -1,8 +0,0 @@
-from eva_data_analysis import text_to_duration
-
-def test_text_to_duration_float():
-    assert abs(text_to_duration("10:20") - 10.333) < 1e5
-
-def test_text_to_duration_integer():
-    input_value = "10:00"
-    assert text_to_duration(input_value) == 10
